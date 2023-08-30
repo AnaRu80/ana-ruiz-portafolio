0320a0c657f8a86efe111c1864bee12cbe17c2fa
Ana Ruiz <ruizana80@gmail.com>
2023-08-30 10:45:04 -0600

    new branch test

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -9,7 +9,7 @@ export function AboutPage() {
           I am a  a former electronic engineer who found her true passion in development. With over five years of experience, I've gone from crafting user interfaces in React to building more extensive applications using Node.js.<br /><br /> My journey also led me to master React Native, enabling me to create captivating mobile apps. Beyond the screen, you'll find me engrossed in series, exploring new places, or dancing salsa. Let's collaborate and turn ideas into reality!
         </p>
       </div>
-      <div className="flex justify-center items-center sm:w-1/2 ">
+      <div className="flex justify-center items-center sm:w-1/2">
         <img
           src={WomanDevelop}
           alt='Woman developing'
